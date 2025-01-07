ef560ab230e26cf7bab098c9d05f53fda559ac9c
bsashabbb <bsashabbb@yandex.ru>
2025-01-07 20:23:36 +0300

    Добавлены файлы admins.json, bans.json, bot.py, contexts.json, funcs_for_resp.py, generate.py, keys.json, prompt_groups.json, prompts.json, prompts_message_ids.json, settings.json, users.json

---
+++ funcs_for_resp.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+def add(a: float, b: float):
+    """returns a + b."""
+    return a + b
+
+
+def substract(a: float, b: float):
+    """returns a - b."""
+    return a - b
+
+
+def divide(a: float, b: float):
+    """returns a / b."""
+    return a / b
+
+
+def multiply(a: float, b: float):
+    """returns a * b."""
+    return a * b
+
+
+def datetime_now():
+    """returns datetime now."""
+    return datetime.now()
